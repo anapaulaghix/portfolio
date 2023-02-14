8a49ddd601ac813641ec0fe4a9565a8aa007fb3a
anapaula <anapaulaghis@gmail.com>
2023-02-14 16:53:30 -0300

    att html

--- frontend/app.js
+++ frontend/app.js
@@ -9,6 +9,7 @@ for( var checkbox of checkboxes){
     checkbox.addEventListener('click', function(){
         if(this.checked == true){
             console.log(this.value)
+            listArray.push(this.value)
         } else {
             console.log("unchecked")
         }
