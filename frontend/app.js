81fd234a8a6b84350feffc81c65a842aee24e56c
anapaula <anapaulaghis@gmail.com>
2023-02-14 13:22:34 -0300

    update js

---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+console.log("linked")
+
+var valuelist = document.getElementById('valuelist');
+var listArray = [];
+var checkboxes = document.querySelectorAll('.checkbox')
+console.log(checkboxes)
+
+for( var checkbox of checkboxes){
+    checkbox.addEventListener('click', function(){
+        if(this.checked == true){
+            console.log(this.value)
+        } else {
+            console.log("unchecked")
+        }
+    })
+}
